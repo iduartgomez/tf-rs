f933061ac803c6232ff0afec72047e77d75fe85a
IDG <iduartgomez@gmail.com>
2017-11-02 10:15:32 +0100

    fix: add GetIdent trait to pub exports

--- src/lib.rs
+++ src/lib.rs
@@ -14,19 +14,19 @@ mod errors;
 #[macro_use]
 pub mod ops;
 pub mod train;
-pub mod client;
+pub(crate) mod client;
 pub(crate) mod framework;
 
 use tf::{OperationDescription, Output, Shape};
 pub type OperationData = tf::Operation;
 pub type TypedTensor<T> = tf::Tensor<T>;
 use tf::{DataType, Graph, Session, SessionOptions, StepWithGraph};
-use tf::{QUInt8, QInt16, QUInt16, QInt32, BFloat16};
+use tf::{BFloat16, QInt16, QInt32, QUInt16, QUInt8};
 use num_complex::{Complex32, Complex64};
 
 pub mod prelude {
-    pub use super::framework::{Attribute, Constant, DefinedShape, NodeIdent, Operation, Scope,
-                               Tensor, TensorArray, TensorContent, Variable};
+    pub use super::framework::{Attribute, Constant, DefinedShape, GetIdent, NodeIdent, Operation,
+                               Scope, Tensor, TensorArray, TensorContent, Variable};
     pub use super::client::ClientSession;
     pub use super::{OperationData, TypedTensor};
     pub use super::errors::Error as TFError;
